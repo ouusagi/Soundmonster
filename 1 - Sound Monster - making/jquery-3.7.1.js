e907a3c77c10917511d834e9b42ef6e0fa470e05
Cherry ₍ᵔ..ᵔ₎ <xmflzhqxj123@naver.com>
2025-03-19 20:30:38 +0900

    사운드 몬스터 1
    작업중

---
+++ 1 - Sound Monster - making/jquery-3.7.1.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+	spaceBetween:10,
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+
+  });
